8b43568e9f0ce78ef240fb9fe2c2234c112db3d6
Danye Jia <danye.jia@morningstar.com>
2019-10-02 10:15:35 -0500

    update

---
+++ aria2c/aria2-init-d-gen.sh
@@ -0,0 +1,43 @@
+cat > /etc/init.d/aria2c << "EOF"
+#! /bin/sh
+# /etc/init.d/aria2c
+ 
+### BEGIN INIT INFO
+# Provides: aria2cRPC
+# Required-Start: $network $local_fs $remote_fs
+# Required-Stop: $network $local_fs $remote_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: aria2c RPC init script.
+# Description: Starts and stops aria2 RPC services.
+### END INIT INFO
+ 
+RETVAL=0
+case "$1" in
+ start)
+ echo -n "Starting aria2c daemon: "
+ umask 0000
+ aria2c --conf-path=/home/pi/.aria2c/aria2.conf -D
+ RETVAL=$?
+ echo
+ ;;
+ stop)
+ echo -n "Shutting down aria2c daemon: "
+ /usr/bin/killall aria2c
+ RETVAL=$?
+ echo
+ ;;
+ restart)
+ stop 
+ sleep 3
+ start
+ ;;
+ *)
+ echo $"Usage: $0 {start|stop|restart}"
+ RETVAL=1
+esac
+exit $RETVAL
+EOF
+
+chmod +x /etc/init.d/aria2c
+update-rc.d aria2c defaults
---
+++ intellij/config.md
@@ -0,0 +1,5 @@
+
+## background color when click a word ##
+
+Preference -> Editor > Color Scheme -> General -> Code -> Identifier under caret, Background, e.g. 5D5D5D
+
