c3e3cb850ab4b8413dae95093855ec46d5a0cc8e
Danye Jia <djia@DELL-T40.attlocal.net>
2020-08-05 10:39:28 -0500

    update

--- scripts/00.init.sh
+++ scripts/00.init.sh
@@ -2,12 +2,20 @@
 
 dnf update -y
 dnf install -y git zsh epel-release
-dnf install -y @virt
+dnf install -y @virt virt-install
 dnf install -y cockpit cockpit-machines
 
 systemctl start cockpit.socket
 systemctl enable cockpit.socket
 systemctl status cockpit.socket
 
+systemctl enable libvirtd
+systemctl start libvirtd
+
 firewall-cmd --add-service=cockpit --permanent
 firewall-cmd --reload
+
+nmcli con add type bridge con-name br0 ifname br0
+nmcli con add type ethernet slave-type bridge con-name bridge-br0 ifname eno1 master br0
+nmcli con up br0
+nmcli con down eno1
