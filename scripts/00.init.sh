39f45cba3628aa706aae3735c88db69d7aa05b4c
Danye Jia <djia@DELL-T40.attlocal.net>
2020-08-05 07:11:02 -0500

    update

--- scripts/00.init.sh
+++ scripts/00.init.sh
@@ -2,3 +2,12 @@
 
 dnf update -y
 dnf install -y git zsh epel-release
+dnf install -y @virt
+dnf install -y cockpit cockpit-machines
+
+systemctl start cockpit.socket
+systemctl enable cockpit.socket
+systemctl status cockpit.socket
+
+firewall-cmd --add-service=cockpit --permanent
+firewall-cmd --reload
