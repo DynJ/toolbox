74a01e73e0c862f40414c9dcbb35b8dbc230a0ad
Danye Jia <danye.jia@morningstar.com>
2020-08-07 07:09:08 -0500

    --wip-- [skip ci]

--- scripts/00.init.sh
+++ scripts/00.init.sh
@@ -19,3 +19,10 @@ nmcli con add type bridge con-name br0 ifname br0
 nmcli con add type ethernet slave-type bridge con-name bridge-br0 ifname eno1 master br0
 nmcli con up br0
 nmcli con down eno1
+
+# current issue
+# virtual os can't get ip, 
+# I once use VNC and virt-manager, the win10 can be remoonce use VNC and virt-manager, the win10 can be remoted.
+# but when I use cocopit to create kvm, the omv can't ping google and it has no once use VNC and virt-manager, the win10 can be remoted.
+# virsh net-list can't show anything. ref: https://www.cyberciti.biz/faq/find-ip-address-of-linux-kvm-guest-virtual-machine/
+# not sure if virsh should show something. I didn't try it when use win10 kvm
