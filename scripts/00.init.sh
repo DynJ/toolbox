130f528c8a0595b546ec11d72a30771c3f804b53
Danye Jia <danye.jia@morningstar.com>
2020-08-10 07:46:06 -0500

    --wip-- [skip ci]

--- scripts/00.init.sh
+++ scripts/00.init.sh
@@ -26,3 +26,20 @@ nmcli con down eno1
 # but when I use cocopit to create kvm, the omv can't ping google and it has no once use VNC and virt-manager, the win10 can be remoted.
 # virsh net-list can't show anything. ref: https://www.cyberciti.biz/faq/find-ip-address-of-linux-kvm-guest-virtual-machine/
 # not sure if virsh should show something. I didn't try it when use win10 kvm
+# ideas: 1. config static ip?
+
+# ref: 
+# https://www.cyberciti.biz/faq/centos-8-add-network-bridge-br0-with-nmcli-command/
+# https://www.linuxtechi.com/install-kvm-hypervisor-on-centos-7-and-rhel-7/
+# https://www.tecmint.com/create-network-bridge-in-rhel-centos-8/
+# https://www.tecmint.com/install-kvm-in-centos-8/
+# https://computingforgeeks.com/how-to-create-and-configure-bridge-networking-for-kvm-in-linux/
+# https://computingforgeeks.com/how-to-create-a-linux-network-bridge-on-rhel-centos-8/
+# https://computingforgeeks.com/how-to-install-kvm-on-rhel-8/
+# https://www.golinuxcloud.com/how-to-configure-network-bridge-nmtui-linux/
+
+# how to install docker? --nobest
+
+# virtio for win: 
+# https://www.hiczp.com/linux/wei-kvm-zhong-de-windows-xu-ni-ji-qi-yong-virtio.html
+# https://getlabsdone.com/10-easy-steps-to-install-windows-10-on-linux-kvm/#Install-Windows-virtio-drivers
